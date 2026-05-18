26ec06d1748c2a5fc0b68a2cca682b542a3719ee
Nguyễn Anh Tú <vanhoanh1st@gmail.com>
2026-05-19 02:40:48 +0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from .gru4rec import GRU4Rec
 from .hier_gru import HierGRU
 from .sasrec import SASRec
 from .hier_sasrec import HierSASRec
+from .nextitnet import NextItNet
+from .hier_nextitnet import HierNextItNet
