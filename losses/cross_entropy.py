603dde8b193704636060378e5fc8846519f1c5d5
Nguyễn Anh Tú <vanhoanh1st@gmail.com>
2026-05-19 02:43:00 +0700

    Create cross_entropy.py

---
+++ losses/cross_entropy.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+
+class SequenceCrossEntropyLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, logits, targets):
+        """
+        logits: [B, num_items]
+        targets: [B] or [B, 1]
+        """
+
+        if targets.dim() > 1:
+            targets = targets.squeeze(-1)
+
+        return self.loss_fn(logits, targets)
