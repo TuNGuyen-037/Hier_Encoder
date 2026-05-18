6396bdf46b1d2617c2c5c8619333275e880d9497
Nguyễn Anh Tú <vanhoanh1st@gmail.com>
2026-05-19 02:14:00 +0700

    Create device.py

---
+++ utils/device.py
@@ -0,0 +1,10 @@
+import torch
+
+
+def get_device(device_cfg="auto"):
+    if device_cfg == "auto":
+        return torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+    return torch.device(device_cfg)
