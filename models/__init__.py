2c2c0faea9248e7352f2f5b85e03d3b340cfc23e
Nguyễn Anh Tú <vanhoanh1st@gmail.com>
2026-05-19 03:01:31 +0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,28 @@ from .sasrec import SASRec
 from .hier_sasrec import HierSASRec
 from .nextitnet import NextItNet
 from .hier_nextitnet import HierNextItNet
+
+
+MODEL_REGISTRY = {
+    "gru4rec": GRU4Rec,
+    "hier_gru": HierGRU,
+    "sasrec": SASRec,
+    "hier_sasrec": HierSASRec,
+    "nextitnet": NextItNet,
+    "hier_nextitnet": HierNextItNet,
+}
+
+
+ALL_MODELS = list(MODEL_REGISTRY.keys())
+
+
+__all__ = [
+    "GRU4Rec",
+    "HierGRU",
+    "SASRec",
+    "HierSASRec",
+    "NextItNet",
+    "HierNextItNet",
+    "MODEL_REGISTRY",
+    "ALL_MODELS",
+]
