9e940a13571da618672affa3194ebccfc0df3185
Nguyễn Anh Tú <vanhoanh1st@gmail.com>
2026-06-18 08:44:20 +0700

    Update device.py

--- utils/device.py
+++ utils/device.py
@@ -1,10 +1,12 @@
 # utils/decice.py
-
 import torch
 
 
 def get_device(device_cfg="auto"):
-    if device_cfg == "auto":
+    """
+    Tự động chọn cấu hình phần cứng tối ưu (CUDA / CPU).
+    """
+    if device_cfg == "auto" or device_cfg is None:
         return torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"
         )
