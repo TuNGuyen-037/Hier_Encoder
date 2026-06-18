012f418090781be534ee341e546e80ee981596f2
Nguyễn Anh Tú <vanhoanh1st@gmail.com>
2026-06-18 07:50:05 +0700

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,8 @@
 # datasets/__init__.py
 from .amz2023 import Amazon2023Dataset
-from .dataloader import get_train_loader
+from .dataloader import get_dataloaders
 
 __all__ = [
     "Amazon2023Dataset",
-    "get_train_loader",
+    "get_dataloaders",
 ]
